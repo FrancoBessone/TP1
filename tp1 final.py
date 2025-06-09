e07c42618f00410065383eb997b8520645453bd5
Franco Bessone <franco.trancos@gmail.com>
2025-06-09 18:41:13 -0300

    Add files via upload

---
+++ tp1 final.py
@@ -0,0 +1,47 @@
+from typing import Optional, List
+from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
+
+class Mensaje(BaseModel):
+        id: Optional[int] = None
+        user: str
+        mensaje: str
+
+app = FastAPI()
+
+mensajes_db: List[Mensaje] = []
+@app.post("/mensajes", response_model=Mensaje)
+def crear_mensaje(mensaje: Mensaje):
+    mensaje.id = len(mensajes_db) + 1
+    mensajes_db.append(mensaje)
+    return mensaje
+
+@app.get("/mensajes/{mensaje_id}", response_model=Mensaje)
+def obtener_mensaje(mensaje_id: int):
+    for mensaje in mensajes_db:
+        if mensaje.id == mensaje_id:
+            return mensaje
+    raise HTTPException(status_code=404, detail="Mensaje no encontrado")
+
+@app.get("/mensajes", response_model=List[Mensaje])
+def listar_mensajes():
+    return mensajes_db
+
+@app.put("/mensajes/{mensaje_id}", response_model=Mensaje)
+def actualizar_mensaje(mensaje_id: int, mensaje_actualizado: Mensaje):
+    for index, mensaje in enumerate(mensajes_db):
+        if mensaje.id == mensaje_id:
+            mensaje_actualizado.id = mensaje_id
+            mensajes_db[index] = mensaje_actualizado
+        return mensaje_actualizado
+    raise HTTPException(status_code=404, detail="Mensaje no encontrado para actualizar")
+
+@app.delete("/mensajes/{mensaje_id}", response_model=dict)
+def eliminar_mensaje(mensaje_id: int):
+    for index, mensaje in enumerate(mensajes_db):
+        if mensaje.id == mensaje_id:
+            del mensajes_db[index]
+            return {"detail": "Mensaje eliminado"}
+            raise HTTPException(status_code=404, detail="Mensaje no encontrado para eliminar")
+        
+    
